c613fc6d3545ca9d3376b6484ddcd7fef8db2bc1
Liby Lee <liby99@icloud.com>
2018-11-04 03:00:33 -0800

    Cleaned up files

---
+++ src/test/lib/unit_test.js
@@ -0,0 +1,30 @@
+module.exports = function (obj) {
+    
+    function afterBegin () {
+        
+        (function t(i) {
+            if (i < obj.tests.length) {
+                obj.tests[i](() => {
+                    t(i + 1);
+                }, (err) => {
+                    if (err) {
+                        console.error(err);
+                    }
+                    afterTest();
+                });
+            }
+            else {
+                afterTest();
+            }
+        })(0);
+    }
+    
+    function afterTest() {
+        if (obj.finish) obj.finish();
+    }
+    
+    if (obj.begin)
+        obj.begin(afterBegin);
+    else
+        afterBegin();
+};
