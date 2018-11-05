f6bd3369be35f0d2d15fb4ab878f510de8218314
Liby Lee <liby99@icloud.com>
2018-11-04 23:04:37 -0800

    Fixed linter script. Again

--- src/test/lib/unit_test.js
+++ src/test/lib/unit_test.js
@@ -1,30 +1,24 @@
 module.exports = function (obj) {
-    
-    function afterBegin () {
-        
-        (function t(i) {
-            if (i < obj.tests.length) {
-                obj.tests[i](() => {
-                    t(i + 1);
-                }, (err) => {
-                    if (err) {
-                        console.error(err);
-                    }
-                    afterTest();
-                });
-            }
-            else {
-                afterTest();
-            }
-        })(0);
-    }
-    
-    function afterTest() {
-        if (obj.finish) obj.finish();
-    }
-    
-    if (obj.begin)
-        obj.begin(afterBegin);
-    else
-        afterBegin();
-};
+  function afterBegin () {
+    (function t (i) {
+      if (i < obj.tests.length) {
+        obj.tests[i](() => {
+          t(i + 1)
+        }, (err) => {
+          if (err) {
+            console.error(err)
+          }
+          afterTest()
+        })
+      } else {
+        afterTest()
+      }
+    })(0)
+  }
+
+  function afterTest () {
+    if (obj.finish) obj.finish()
+  }
+
+  if (obj.begin) { obj.begin(afterBegin) } else { afterBegin() }
+}
